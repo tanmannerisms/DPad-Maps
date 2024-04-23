d9e4bd8911b8e697a4d88e7e7eae521c83302547
Tanner Mills <tanner.mills2000@gmail.com>
2024-04-23 16:47:01 -0600

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            gradlePluginPortal()
+            google()
+            mavenCentral()
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DPad Maps"
+include(":app")
